a644663803437389f03b3f48bc8c9bf36247679f
zzz <zzz@qq.com>
2023-04-15 02:23:46 +0800

    7——Spring入门

---
+++ spring_helloworld/src/main/java/com/learn/spring/pojo/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.learn.spring.pojo;
+
+public class HelloWorld {
+    public void sayHello() {
+        System.out.println("hello,Spring");
+    }
+}
---
+++ spring_helloworld/src/test/java/com/learn/spring/test/HelloWorldTest.java
@@ -0,0 +1,17 @@
+package com.learn.spring.test;
+
+import com.learn.spring.pojo.HelloWorld;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloWorldTest {
+    @Test
+    public void test() {
+        //获取IOC容器
+        ApplicationContext ioc = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //获取IOC容器中的bean
+        HelloWorld helloworld = (HelloWorld) ioc.getBean("helloworld");
+        helloworld.sayHello();
+    }
+}
